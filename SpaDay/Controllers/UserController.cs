43cdeff77cc2d4a2e0da04c40c55415a72fc3e8d
Charlie <Charlie@Carde-MacBook.local>
2021-07-26 12:39:37 -0400

    Completed Studio Chpt14

---
+++ SpaDay/Controllers/UserController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SpaDay.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SpaDay.Controllers
+{
+    public class UserController : Controller
+    {
+        
+        // GET: /<controller>/
+        [Route("/User/Add")]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        public IActionResult Index ()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/user")]
+        public IActionResult SubmitAddUserForm(User newUser, string verify)
+        {
+            // add form submission handling code here
+            if (newUser.Password == verify)
+            {
+                ViewBag.newPerson = newUser;
+                return View("Index");
+            }
+            else
+                @ViewBag.username = newUser.UserName;
+                @ViewBag.email = newUser.Email;
+                ViewBag.ErrorMessage = "Password does not match, please try again.";
+                return View("add");
+        }
+    }
+}
---
+++ SpaDay/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace SpaDay.Models
+{
+    public class User
+    {
+
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+
+        
+        public User(string username, string password, string email)
+        {
+            this.UserName = username;
+            this.Password = password;
+            this.Email = email;
+        }
+
+        public User()
+        {
+
+        }
+    }
+}
